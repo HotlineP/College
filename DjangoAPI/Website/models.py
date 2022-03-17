2080e6e4a91b24dd4190778fbf7a80e4e79d7585
Pietro <92400353+HotlineP@users.noreply.github.com>
2022-03-17 13:15:30 -0300

    bruh

--- DjangoAPI/Website/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class User(models.Model):
-    UserID = models.AutoField(primary_key=True)
-    UserName = models.CharField(max_length=50)
-    DateOfJoining = models.DateField(max_length=10)
