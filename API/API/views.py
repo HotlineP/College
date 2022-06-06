7c4b8bf722f24894e1f24a35d4d65bfb8e4dcc9f
Pietro <92400353+HotlineP@users.noreply.github.com>
2022-06-06 15:56:28 -0300

    initial commit

--- API/API/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-porra = 'templates/main/index.html'
-
-def main(request):
-    return render(request, 'templates/main/index.html')
-    #return HttpResponse("<h1>seja bem vindo bruh</h1>")
